52bbf44d953700b8c922160080c90f469799f171
Nick T <93458816+nickt28@users.noreply.github.com>
2023-08-19 22:10:31 +1000

    Graph
    hackey

--- maps_2/maps.js
+++ maps_2/maps.js
@@ -9,10 +9,15 @@ google.charts.load('current', {
 
     const inputData = [
         ['Lat', 'Long', 'Population'],
-        [37.4232, -122.0853, 150],
-        [40.7128, -74.0060, 200],
-        [51.5074, -0.1278, 250],
-        [35.6895, 139.6917, 300]
+        [-37.8152065, 144.963937, 2109.576],
+        [-37.8161444, 144.9804594, 720.069],
+        [-37.8114504, 144.9253974, 1677.48],
+        [-37.8301583, 144.9804594, 370.96],
+        [-37.8245483, 144.963937, 1186.465],
+        [-37.8170652, 144.9419122, 1594.805],
+        [-37.7960195, 144.9006313, 6299.184],
+        [-37.8146251, 144.8456217, 10441.392],
+        [-37.819811, 144.8813738, 5723.072]
       ];
 
    var data = google.visualization.arrayToDataTable(inputData);
@@ -20,7 +25,8 @@ google.charts.load('current', {
    var options = {
      region: 'AU',
      displayMode: 'markers',
-     colorAxis: {colors: ['green', 'blue']}
+     colorAxis: {colors: ['green', 'blue']},
+     resolution: 'provinces'
    };
 
    var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));
