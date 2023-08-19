af5b8937daeda026fc6166ec6eae52a35cb1c0cf
Nick T <93458816+nickt28@users.noreply.github.com>
2023-08-20 02:35:49 +1000

    G maps data plotting

--- maps_3/maps.js
+++ maps_3/maps.js
@@ -1,40 +1,63 @@
-import data from './solar.json' assert { type: 'json' };
+import solarData from './solar.json' assert { type: 'json' };
+
+function geoEnc(data) {
+  const features = data.map((item) => {
+      return {
+      type: "Feature",
+      geometry: {
+          type: "Point",
+          coordinates: [item.longitude, item.latitude],
+      },
+      properties: {
+          iqt: item["Installations Quantity Total"],
+          kw: item["SGU Rated Output In kW Total"],
+          postcode: item.postcode
+      },
+      };
+  });
+  
+  return {
+      type: "FeatureCollection",
+      features: features
+  }
+}
 
 let map;
 
 function initMap() {
-  console.log(data);
-    
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 20, lng: -160 },
-    zoom: 2,
+    center: { lat: -37.840935, lng: 144.946457 },
+    zoom: 9,
   });
   
-  const script = document.createElement("script");
-
-  script.setAttribute(
-    "src",
-    "https://storage.googleapis.com/mapsdevsite/json/quakes.geo.json",
-  );
-  document.getElementsByTagName("head")[0].appendChild(script);
+  const pdata = geoEnc(solarData);
+  map.data.addGeoJson(pdata);
+  
+  // const script = document.createElement("script");
+  // script.setAttribute(
+  //   "src",
+  //   "https://storage.googleapis.com/mapsdevsite/json/quakes.geo.json",
+  // );
+  // document.getElementsByTagName("head")[0].appendChild(script);
 
   map.data.setStyle((feature) => {
-    const mag = Math.exp(parseFloat(feature.getProperty("mag"))) * 0.1;
+    const dScale = Math.pow(Math.log(feature.getProperty("iqt")),1.4)
     return {
       icon: {
         path: google.maps.SymbolPath.CIRCLE,
-        scale: mag,
+        scale: dScale,
         fillColor: "#f00",
         fillOpacity: 0.35,
-        strokeWeight: 0,
+        strokeColor: "#444",
+        strokeWeight: 1,
       },
     };
   });
 }
 
-function eqfeed_callback(data) {
-  map.data.addGeoJson(data);
-}
+// function eqfeed_callback(data) {
+//   map.data.addGeoJson(data);
+// }
 
 window.initMap = initMap;
-window.eqfeed_callback = eqfeed_callback;
+// window.eqfeed_callback = eqfeed_callback;
