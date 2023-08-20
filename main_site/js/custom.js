935fcb6c3b93647651c59d32e08a39e3dcca9145
Nick T <93458816+nickt28@users.noreply.github.com>
2023-08-20 13:59:43 +1000

    Merge

--- main_site/js/custom.js
+++ main_site/js/custom.js
@@ -1,17 +1,3 @@
-var hero = $(".hero_area");
-var menu = $(".custom_menu-container");
-$(window).scroll(function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > hero.height()) {
-        menu.addClass("menu_fixed-position");
-        $(".custom_menu-container + section").addClass("mt-5");
-    } else {
-        menu.removeClass("menu_fixed-position");
-        $(".custom_menu-container + section").removeClass("mt-5");
-    }
-});
-
-
 var myCircle = Circles.create({
     id: "circles-1",
     radius: 65,
